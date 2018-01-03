6ea0b9adcacc92f2f92e267aa5161c356369ec20
Oleksii Shevchuk <alxchk@gmail.com>
2018-01-03 15:04:36 +0200

    Initial commit

---
+++ linux32/bootstrap.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+export PATH=/sbin:/usr/sbin:/usr/local/sbin:/bin:/usr/bin:/usr/local/sbin:$HOME/.local/bin
+
+set -x
+set -e
+
+# ENV
+BUILDENV=`dirname $0`
+LIN32=$BUILDENV/rootfs
+
+debootstrap --arch i386 woody $LIN32 http://archive.debian.org/debian
+mkdir -p $LIN32/usr/src
+
+mkdir -p $LIN32/etc/ssl/certs
+cp -L /etc/ssl/certs/*.0 $LIN32/etc/ssl/certs/
+cp /etc/ssl/certs/ca-* $LIN32/etc/ssl/certs/
+cat /etc/resolv.conf >$LIN32/etc/resolv.conf
+
+cat > $LIN32/wrap.c <<EOF
+#define _GNU_SOURCE
+#include <sys/utsname.h>
+#include <string.h>
+
+static const
+struct utsname Build_utsname = {
+  .sysname = "Linux",
+  .nodename = "Build",
+  .release = "2.4.0",
+  .version = "2.4.0",
+  .machine = "i386",
+  .domainname = "Build",
+} ;
+
+int uname(struct utsname *buf) {
+    memcpy(buf, &Build_utsname, sizeof(struct utsname));
+    return 0;
+}
+EOF
+
+cat > $LIN32/gccwrap <<EOF
+#!/bin/bash
+declare -a filter=( "\$CFLAGS_FILTER" )
+declare -a outargs=()
+
+for arg; do
+  found=false
+  for filtered in \${filter[@]}; do
+     if [ "\$filtered" == "\$arg" ]; then
+        found=true
+        break
+     fi
+  done
+
+  if [ "\$found" = "false" ]; then
+        outargs[\${#outargs[@]}]="\$arg"
+  fi
+
+done
+
+exec gcc "\${outargs[@]}"
+EOF
+
+chmod +x $LIN32/gccwrap
+
+cat <<__CMDS__ > $LIN32/deploy.sh
+
+export LC_ALL=C
+export TERM=
+export DEBIAN_FRONTEND=noninteractive
+/bin/sh -c "apt-get --force-yes -y install gcc-3.0 make libc-dev \
+ perl m4 gettext libexpat1-dev flex bison file libstdc++2.10-dev \
+ libtool patch xutils xlibs-dev < /dev/null"
+
+cd /
+gcc -fPIC -o /wrap.so -shared /wrap.c
+echo /wrap.so >/etc/ld.so.preload
+
+mkdir /opt/static
+ln -sf /usr/lib/gcc-lib/i386-linux/3.0.4/libgcc.a /opt/static/
+ln -sf /usr/lib/libffi.a /opt/static/
+ln -sf /usr/lib/libutil.a /opt/static/
+ln -sf /usr/bin/gcc-3.0 /usr/bin/gcc
+ln -sf /usr/bin/gcc-3.0 /usr/bin/cc
+ln -sf /usr/X11R6/lib/libX11.so /usr/lib/
+ln -sf /usr/X11R6/lib/libXss.a /usr/lib/
+apt-get clean
+
+rm -f /etc/resolv.conf
+rm -f /deploy.sh
+rm -f /wrap.c
+
+ldconfig
+__CMDS__
+
+mkdir -p $LIN32/proc
+mkdir -p $LIN32/dev
+mount -t proc proc $LIN32/proc
+mount -t devtmpfs devtmpfs $LIN32/dev
+
+chroot $LIN32 /bin/bash -x /deploy.sh
+
+umount $LIN32/proc
+umount $LIN32/dev
+
+tar -C $LIN32 -c . | docker import - linux32
---
+++ linux64/bootstrap.sh
@@ -0,0 +1,111 @@
+#!/bin/sh
+
+set -x
+set -e
+
+SELF=`readlink -f "$0"`
+LIN64=`dirname "$SELF"`/rootfs
+debootstrap --no-check-gpg --arch amd64 etch $LIN64 http://archive.debian.org/debian
+
+mkdir -p $LIN64/etc/ssl/certs
+for file in /etc/ssl/certs/*.0; do
+    cat $file >$LIN64/etc/ssl/certs/`basename "$file"`
+done
+
+cp /etc/ssl/certs/ca-* $LIN64/etc/ssl/certs/
+cat /etc/resolv.conf >$LIN64/etc/resolv.conf
+
+cat > $LIN64/wrap.c <<EOF
+#define _GNU_SOURCE
+#include <sys/utsname.h>
+#include <string.h>
+
+static const
+struct utsname Build_utsname = {
+  .sysname = "Linux",
+  .nodename = "Build",
+  .release = "2.4.0",
+  .version = "2.4.0",
+  .machine = "x86_64",
+  .domainname = "Build",
+};
+
+int uname(struct utsname *buf) {
+    memcpy(buf, &Build_utsname, sizeof(struct utsname));
+    return 0;
+}
+EOF
+
+cat > $LIN64/gccwrap <<EOF
+#!/bin/bash
+declare -a filter=( "\$CFLAGS_FILTER" )
+declare -a badargs=( "\$CFLAGS_ABORT" )
+declare -a outargs=()
+
+for arg; do
+  found=false
+  for filtered in \${filter[@]}; do
+     if [ "\$filtered" == "\$arg" ]; then
+        found=true
+        break
+     fi
+  done
+
+  for bad in \${badargs[@]}; do
+     if [ "\$bad" == "\$arg" ]; then
+        echo "Unsupported argument found: \$bad"
+        exit 1
+     fi
+  done
+
+  if [ "\$found" = "false" ]; then
+        outargs[\${#outargs[@]}]="\$arg"
+  fi
+
+done
+
+exec gcc "\${outargs[@]}"
+EOF
+
+chmod +x $LIN64/gccwrap
+
+cat <<__CMDS__ > $LIN64/deploy.sh
+
+set -x
+
+export LC_ALL=C
+export TERM=
+export DEBIAN_FRONTEND=noninteractive
+
+/bin/sh -c "apt-get --force-yes -y install build-essential make libc-dev \
+ perl m4 gettext libexpat1-dev flex bison file libtool patch xutils \
+ libx11-dev libxss-dev < /dev/null"
+
+cd /
+gcc -fPIC -o /wrap.so -shared /wrap.c
+echo /wrap.so >/etc/ld.so.preload
+
+mkdir /opt/static
+ln -sf /usr/lib/gcc/x86_64-linux-gnu/4.1.2/libgcc.a /opt/static
+ln -sf /usr/lib/gcc/x86_64-linux-gnu/4.1.2/libssp.a /opt/static
+ln -sf /usr/lib/gcc/x86_64-linux-gnu/4.1.2/libssp_nonshared.a /opt/static
+ln -sf /usr/lib/libffi.a /opt/static/
+
+rm -f /wrap.c
+rm -f /deploy.sh
+apt-get clean
+ldconfig
+
+__CMDS__
+
+mkdir -p $LIN64/proc
+mkdir -p $LIN64/dev
+mount -t proc proc $LIN64/proc
+mount -t devtmpfs devtmpfs $LIN64/dev
+
+chroot $LIN64 /bin/bash -x /deploy.sh
+
+umount $LIN64/proc
+umount $LIN64/dev
+
+tar -C $LIN64 -c . | docker import - linux64
