f79fb4b2f1356b26dc13b4a8899e694406038757
Oleksii Shevchuk <alxchk@gmail.com>
2021-03-21 17:31:29 +0200

    windows: Reduce size of msvs-slim image

--- windows/bootstrap.sh
+++ windows/bootstrap.sh
@@ -6,10 +6,10 @@ DOCKER_COMMAND=${DOCKER_COMMAND:-docker}
 
 ${DOCKER_COMMAND} build --squash -f Dockerfile.buster-wine -t debian:buster-wine
 
-${DOCKER_COMMAND} build --squash -t wine:debian -f Dockerfile.wine-slim .
+${DOCKER_COMMAND} build --squash -t wine:debian -f Dockerfile.wine-debian .
 
 if [ ! -d MSVCDocker/build/msvc15/snapshots/CMP ]; then
     make -C MSVCDocker WINE_VER=debian MSVC_VERS=15 DOCKERCMD=${DOCKER_COMMAND}
 fi
 
-${DOCKER_COMMAND} build --squash -t msvc-slim:15 -f Dockerfile.slim MSVCDocker
+${DOCKER_COMMAND} build --squash -t msvc-slim:15 -f Dockerfile.msvs-slim MSVCDocker
